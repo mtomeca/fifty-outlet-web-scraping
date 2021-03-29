2041db21b707edc5a936118e48ca57c513c9e04d
mtomeca <76868207+mtomeca@users.noreply.github.com>
2021-03-29 08:20:04 +0200

    Variable currPrice afegida
    He comentat l'html.parser!

--- PRA1_codi.py
+++ PRA1_codi.py
@@ -29,13 +29,20 @@ links_list = []
 first_Price = "_306iTrToX_OShtSAP7XevK _1Y-4pH1BWwMV4UMjEhAG36"
 firstPrice_list = []
 
+# Classe on apareix e preu actual del producte.
+curr_Price_class = "_2S2si9T7Qd3avRwavIc7Fo m8dZQhR-BlEOUvpj0ciDk" +\
+                   " _35S_lNpYfl-bzhzNIldqTR JMJtx_uzL1tBgGN3qpKJf " +\
+                   "js-product-price"
+currPrice_list = []
+
 # Iteració per emmagatzemar les dades.
 for row in rows:
     a_rows = row.find_all("a", name_attr)
     spans = row.find_all("span", {"class": brand_attr})
     links = row.find_all("a", {"class": name_attr})
     firstPrices = row.find_all("span", {"class": first_Price})
-
+    currentPrices = row.find_all("span", {"class": curr_Price_class})
+    
     for span in spans:
         brands.append(span.text.replace("\n", "").replace(" ", ""))
     for a_row in a_rows:
@@ -44,6 +51,8 @@ for row in rows:
         links_list.append(link['href'])
     for firstPrice in firstPrices:
         firstPrice_list.append(firstPrice.text)
+    for currPrice in currentPrices:
+        currPrice_list.append(currPrice['data-price'])
         
 
 car_price = row.find(class_ = "UsayrQmgXA1LiQJ2Isauz _2Tf_MA-3j5q-XdElDrZ64N").text.strip()
@@ -53,7 +62,7 @@ car_price = row.find(class_ = "UsayrQmgXA1LiQJ2Isauz _2Tf_MA-3j5q-XdElDrZ64N").t
 # print(f"Les marques són:\n{brands, len(brands)}")
 # print(names)
 # print(links_list)
-print(firstPrice_list, len(firstPrice_list))
+#print(firstPrice_list, len(firstPrice_list))
 # print(rows[3])
 
 
